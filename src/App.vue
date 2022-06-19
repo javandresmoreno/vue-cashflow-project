ef57c9b77d2a26bc9eaa97594ad2160de7ef04ab
Javier Moreno <javier@solido.co>
2022-06-18 23:26:43 -0500

    Agrega componente SplashScreen con Suspense

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <HomeComponent />
+  <Suspense>
+    <template #default>
+      <HomeComponent />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import HomeComponent from "./components/HomeComponent.vue";
+import SplashScreen from "./components/SplashScreen.vue";
+
+import { defineAsyncComponent } from "vue";
   
-  export default {
-    components: {
-      HomeComponent
-    }
+export default {
+  components: {
+    SplashScreen,
+    HomeComponent: defineAsyncComponent(() => 
+      new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(import("./components/HomeComponent.vue"));
+        }, 2500);
+      })
+    )  
   }
+}
 </script>
 
 <style>
+  html,
+  body,
+  .app {
+    min-height: 100vh;
+    margin: 0;
+    font-family: 'system-ui', Helvetica, sans-serif;
+  }
+  * {
+    --brand-green: #04b500;
+    --brand-blue: #0689b0;
+  }
 </style>
